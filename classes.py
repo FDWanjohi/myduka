193e695183bf0b7e2e497130a8e7651ebd003124
FDWanjohi <fred@thrill.co.ke>
2026-04-16 19:08:38 +0300

    update

--- classes.py
+++ classes.py
@@ -31,3 +31,37 @@ person2.walks()
 #1. Create a class calls BankAccount with the following attributes: -account number - balance -owner name -date opened
 #2. Give the above BankAccount class the following behavior or methods: -deposit() -withdraw() -display_info()
 #3. Create two BankAccount objects that can deposit, withdraw and display_info
+
+class BankAccount:
+    def __init__(self, account_number, balance, owner_name, date_opened):
+        self.account_number = account_number
+        self.balance = balance
+        self.owner_name = owner_name
+        self.date_opened = date_opened
+
+        def deposit(self):
+            print(f"{self.account_number} deposit")
+
+        def withdraw(self):
+            print(f"{self.account_number} withdraw")
+
+        def display_info(self):
+            print(f"{self.account_number} display_info")
+
+
+BankAccount1 = BankAccount(01234567,1234567,"Ell Ell","May_31_2020")
+print(type(BankAccount1))
+BankAccount1.deposit()
+BankAccount1.withdraw()
+BankAccount1.display_info()
+
+BankAccount2 = BankAccount(02234567,2234567,"Toria Toria","Jul_31_2020")
+print(type(BankAccount2))
+BankAccount2.deposit()
+BankAccount2.withdraw()
+BankAccount2.display_info()
+
+
+
+
+        
